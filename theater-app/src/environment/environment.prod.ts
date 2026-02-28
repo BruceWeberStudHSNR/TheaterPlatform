2f6b19134b95d650940f3ea0690e631c6d05f7fd
Bruce Weber <bruce.weber@stud.hn.de>
2026-02-28 16:59:52 +0100

    also prod env

---
+++ theater-app/src/environment/environment.prod.ts
@@ -0,0 +1,14 @@
+import { Environment } from "./environment.type";
+
+export const environment: Environment  = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDYbC29nisigBh-E95sVJpa3msCJxYDYgg",
+        authDomain: "theater-3d460.firebaseapp.com",
+        projectId: "theater-3d460",
+        storageBucket: "theater-3d460.firebasestorage.app",
+        messagingSenderId: "960539252914",
+        appId: "1:960539252914:web:9474d578927f346bb151ab"
+    }
+
+};
